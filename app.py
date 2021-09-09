fbbc14a91cf63842eca6e286fbc5c30c785facde
tanacchi <q111026d@mail.kyutech.jp>
2021-09-10 01:57:41 +0900

    Show scaatter plot on top page

--- app.py
+++ app.py
@@ -8,27 +8,24 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+Z = np.random.normal(scale=1, size=(100, 2))
+fig = px.scatter(
+    x=Z[:, 0],
+    y=Z[:, 1],
+    range_x=[-3, +3],
+    range_y=[-3, +3],
+    width=800,
+    height=800
+)
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
 
     dcc.Graph(
         id='example-graph',
