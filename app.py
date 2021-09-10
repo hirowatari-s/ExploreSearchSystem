32df25db00d621860f92eb0ec890aa87f2b76601
tanacchi <q111026d@mail.kyutech.jp>
2021-09-10 22:10:34 +0900

    Make animation for scatter plot

--- app.py
+++ app.py
@@ -4,9 +4,11 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
@@ -14,24 +16,40 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-Z = np.random.normal(scale=1, size=(100, 2))
-fig = px.scatter(
-    x=Z[:, 0],
-    y=Z[:, 1],
-    range_x=[-3, +3],
-    range_y=[-3, +3],
-    width=800,
-    height=800
+N = 100
+Z = np.random.normal(scale=1, size=(N, 2))
+zeros = np.zeros(N)
+fig = go.Figure(
+    data=[go.Scatter(x=zeros, y=zeros)],
+    layout=go.Layout(
+       xaxis=dict(range=[-3, +3], autorange=False),
+       yaxis=dict(range=[-3, +3], autorange=False),
+       width=800,
+       height=800,
+       updatemenus=[dict(
+           type="buttons",
+           buttons=[dict(
+               label="Play",
+               method="animate",
+               args=[None]
+           )]
+       )]
+    ),
+    frames=[
+        go.Frame(data=[go.Scatter(x=zeros, y=zeros, mode="markers", marker=dict(size=10))]),
+        go.Frame(data=[go.Scatter(x=Z[:, 0], y=Z[:, 1], mode="markers", marker=dict(size=10))]),
+    ]
 )
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(id='title', children='Hello Dash'),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
-    )
+    ),
 ])
+
 
 import argparse
 parser = argparse.ArgumentParser()
