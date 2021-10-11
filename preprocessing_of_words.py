ba96c45a3c15bd19f947929448a443dc2616701f
hirowatari-s <shirowatari63@gmail.com>
2021-10-11 13:01:53 +0900

    add_make_bow

--- preprocessing_of_words.py
+++ preprocessing_of_words.py
@@ -1,6 +1,9 @@
 import nltk
-import pandas as pd
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import pandas as pd
+import numpy as np
 
 def preprocessing_tag_and_stopwords(s):
     nltk.download('punkt')
@@ -8,7 +11,6 @@ def preprocessing_tag_and_stopwords(s):
 
     need_tag = ['JJ','JJR','JJS','NN','NNS','NNP','NNPS','VB','VBD','VBG','VBN','VBP','VBZ']
     stopWords = set(stopwords.words('english'))
-    print(stopWords)
 
     pos = []
     for i in range(len(s)):
@@ -17,12 +19,25 @@ def preprocessing_tag_and_stopwords(s):
         l_new = []
         for i in l:
             if i[0] not in stopWords and i[1] in need_tag:
-                l_new.append(i[0])
-        pos.append(l_new)
+                l_new.append(i[0]+' ')
+        pos.append(''.join(l_new))
     return pos
+
+def make_bow(df):
+    s = df['snippet']
+    pos = preprocessing_tag_and_stopwords(s)
+
+    vectorizer = CountVectorizer(max_df=50, min_df=3)
+    X = vectorizer.fit_transform(pos)
+    word_label = np.array(vectorizer.get_feature_names())
+    # Tfidf
+    tfidf_transformer = TfidfTransformer(norm='l2', sublinear_tf=True)
+    tfidf = tfidf_transformer.fit_transform(X.toarray())
+
+    features = np.array(tfidf.todense())
+    return features, word_label
 
 if __name__ == '__main__':
     # Load file
     csv_df = pd.read_csv("~/Downloads/Unsupervised Kernel Regression.csv")
-    s = csv_df['snippet']
-    pos = preprocessing_tag_and_stopwords(s)
+    features, word_label = make_bow(csv_df)
