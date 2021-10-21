ec0485ccf77bfecadec8b511140979ef8114a20f
tanacchi <q111026d@mail.kyutech.jp>
2021-10-21 21:28:52 +0900

    Unify body of callback function

--- webapp/event_handler.py
+++ webapp/event_handler.py
@@ -3,24 +3,12 @@ from webapp import app
 import pandas as pd
 from webapp.figure_maker import make_figure
 
+from functools import partial
 
-@app.callback(
-    Output('example-graph', 'figure'),
-    [
-        Input('explore-start', 'n_clicks'),
-        Input('model-selector', 'value'),
-        Input('viewer-selector', 'value'),
-        Input('example-graph2', 'clickData'),
-    ],
-    [
-        State('search-form', 'value'),
-        State('example-graph', 'figure'),
-    ])
 
-def load_learning(n_clicks, model_name, viewer_name, clickData, keyword, prev_fig):
-    print("clicked")
+def load_learning(viewer_id, n_clicks, model_name, viewer_name, clickData, keyword, prev_fig):
+    print("graph clicked")
     print(clickData)
-    print(keyword)
 
     if not keyword:
         keyword = "Machine Learning"
@@ -34,7 +22,20 @@ def load_learning(n_clicks, model_name, viewer_name, clickData, keyword, prev_fi
     return make_figure(keyword, model_name, viewer_name, "viewer_1", None)
 
 
-@app.callback(
+app.callback(
+    Output('example-graph', 'figure'),
+    [
+        Input('explore-start', 'n_clicks'),
+        Input('model-selector', 'value'),
+        Input('viewer-selector', 'value'),
+        Input('example-graph2', 'clickData'),
+    ],
+    [
+        State('search-form', 'value'),
+        State('example-graph', 'figure'),
+])(partial(load_learning, "viewer_1"))
+
+app.callback(
     Output('example-graph2', 'figure'),
     [
         Input('explore-start', 'n_clicks'),
@@ -45,29 +46,7 @@ def load_learning(n_clicks, model_name, viewer_name, clickData, keyword, prev_fi
     [
         State('search-form', 'value'),
         State('example-graph2', 'figure'),
-    ])
-
-def load_learning(n_clicks, model_name, viewer_name, clickData, keyword, prev_fig):
-    if not keyword:
-        keyword = "Machine Learning"
-
-    if clickData:
-        if not ("points" in clickData and "pointIndex" in clickData["points"][0]):
-            pass
-        else:
-            print("clicked_from_map1")
-            return make_figure(keyword, model_name, "CCP", "viewer_2", clickData)
-
-    # return make_figure(keyword, model_name, "viewer_1", viewer_name)
-    return make_figure(keyword, model_name, viewer_name, "viewer_2", None)
-
-
-# def ccp_given_z2(clickData):
-#         print("clicked")
-#         # if not ("points" in Z1 and "pointIndex" in Z1["points"][0]) :
-#         # if clicked viewer_2 variable, change viewer_1
-#         return make_figure("ファッション", "TSOM", "viewer_1", viewer_name="CCP")
-
+])(partial(load_learning, "viewer_2"))
 
 
 @app.callback([
@@ -114,9 +93,8 @@ def update_title(hoverData, keyword, prev_linktext, prev_url, prev_target, prev_
         snippet = ""
     return link_title, url, target, page_title, snippet
 
+
 # app.clientside_callback(
 #     "onLatentClicked",
 #     Output('explore-start', 'outline'),
 #     Input('example-graph', 'clickData'), prevent_initial_call=True)
-
-
