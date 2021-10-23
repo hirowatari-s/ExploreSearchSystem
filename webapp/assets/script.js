e7fc38c95444f67f063f163ef88f405e996bfdad
tanacchi <q111026d@mail.kyutech.jp>
2021-10-23 09:18:41 +0900

    Remove unused code

--- webapp/assets/script.js
+++ webapp/assets/script.js
@@ -1,25 +1,3 @@
-const onLatentClicked = (_) => {
-    const url = document.getElementById("link").href;
-    if (url != "#")
-        window.open(url);
-}
-
-
-const onFirstSearchSubmitted = (e) => {
-    const mainStyle = document.getElementById('main').style;
-    const landingStyle = document.getElementById('landing').style;
-    const paperMapStyle = document.getElementById('paper-map-col').style;
-    const wordMapStyle = document.getElementById('word-map-col').style;
-    const landingSearchKeyword = document.getElementById('landing-search-form').value;
-
-    document.getElementById('search-form').value = landingSearchKeyword;
-    landingStyle.display = 'none';
-    paperMapStyle.display = 'block';
-    wordMapStyle.display = 'block';
-    mainStyle.display = 'block';
-    const searchButton = document.getElementById('explore-start');
-    console.dir(searchButton);
-    // searchButton.onclick();
-
-    return false;
+const sampleFunction = (_) => {
+    console.log('Hello World !')
 }
