5df630e5338c80a023b622186fdc1efdfad7050f
tanacchi <q111026d@mail.kyutech.jp>
2021-10-25 01:07:20 +0900

    Show words on map with out duplication

--- webapp/figure_maker.py
+++ webapp/figure_maker.py
@@ -10,6 +10,7 @@ from sklearn.decomposition import NMF
 from scipy.spatial import distance as dist
 from Grad_norm import Grad_Norm
 from webapp import logger
+from itertools import groupby
 
 
 resolution = 10
@@ -216,6 +217,7 @@ def make_figure(keyword, viewer_name="U_matrix", viewer_id=None, clicked_z=None)
     elif viewer_id == 'viewer_2':
         Z, Y, sigma = history['Z2'], history['Y'], history['sigma']
         labels = labels[1].tolist()
+        logger.debug(f"LABELS: {labels[:5]}")
     else:
         logger.debug("Set viewer_id")
 
@@ -255,8 +257,25 @@ def make_figure(keyword, viewer_name="U_matrix", viewer_id=None, clicked_z=None)
         # u_resolution = 100
         # fig = draw_umatrix(fig, X, Z, sigma, u_resolution, labels)
 
-    if viewer_id == 'viewer_2':
-        pass
+    # Show words when it is highlighted
+    # if viewer_id == 'viewer_2' and not clicked_z == None:
+    #     y = Y[get_bmu(history['Zeta'], clicked_z), :].flatten()
+    #     threshold = float(y.max() * 3 + y.min()) * 0.25  # top 25%
+    #     logger.debug(f"th:{threshold}")
+    #     labels = np.array(labels)
+    #     displayed_zeta = history['Zeta'][y > threshold]
+    #     invisible_z_idx = [idx for idx, z in enumerate(Z) if not np.all([np.invert(np.isclose(z, zeta)) for zeta in displayed_zeta]) ]
+    #     logger.debug(f"invisible_z_idx: {invisible_z_idx}")
+    #     labels[invisible_z_idx] = ''
+    if viewer_id == 'viewer_2' and clicked_z == None:  # FIXME: Remove second condition
+        _, unique_Z_idx = np.unique(Z, axis=0, return_index=True)
+        logger.debug(unique_Z_idx)
+        duplicated_Z_idx = np.setdiff1d(np.arange(Z.shape[0]), unique_Z_idx)
+        # group = groupby(duplicated_Z_idx, key=lambda i: tuple(Z[i]))
+        # invisible_Z_idx = [next(v) for v in group.values()]
+        labels[duplicated_Z_idx] = ''
+
+
 
     fig = draw_scatter(fig, Z, labels, rank, viewer_id)
 
